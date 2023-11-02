913d0aa6cac03c621016d19b432af8d2016f7fa4
user123 <mail>
2023-11-03 00:14:03 +0200

    Initial commit

---
+++ app/src/main/java/farm/favbet/game/fovorit/ChampionHTPL.kt
@@ -0,0 +1,11 @@
+package farm.favbet.game.fovorit
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ChampionHTPL : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_champion_htpl)
+    }
+}
---
+++ app/src/main/java/farm/favbet/game/fovorit/FarmUsAb.kt
@@ -0,0 +1,11 @@
+package farm.favbet.game.fovorit
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class FarmUsAb : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_farm_us_ab)
+    }
+}
